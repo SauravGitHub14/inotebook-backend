4b7d83ea6d24805ce3ed9de63e0048b808023ebb
Saurav <sauravlv123@gmail.com>
2026-07-05 12:33:43 +0530

    Added root route

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "🚀 iNotebook Backend is Live!"
+    });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
