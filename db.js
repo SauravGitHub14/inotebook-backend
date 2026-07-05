070b50689ba1b7ed4accc673a755b055d61191b7
Saurav <sauravlv123@gmail.com>
2026-07-05 12:06:59 +0530

    Added dotenv dependecny

--- db.js
+++ db.js
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose');
-// const mongoURI = "mongodb+srv://saurav:saurav123@cluster0.yhfmevt.mongodb.net/?appName=Cluster0"
-// const mongoURI = "mongodb+srv://saurav_123:Saurav123@cluster0.yhfmevt.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0";
-const mongoURI = "mongodb+srv://saurav_1234:saurav1234@cluster0.ukqx8du.mongodb.net/?appName=Cluster0";
+
+const mongoose = require("mongoose");
+
 const connectToMongo = async () => {
-    await mongoose.connect(mongoURI, {
-        useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    family: 4,})
-    .then( () =>  console.log("Connected to MongoDB successfully"))
-    .catch( (error) => {
-        console.log("Issuse in DB Connection");
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("✅ MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Failed");
         console.error(error.message);
         process.exit(1);
-    })
-       
-    
-}
+    }
+};
 
 module.exports = connectToMongo;
 
